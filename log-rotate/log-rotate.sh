ea1c9228a1e64ac3db618a2d76f41ee647ac2420
Aman Deep <aman.deep307dev@gmail.com>
2024-07-06 23:35:20 +0530

    Update script to create new log file

--- log-rotate/log-rotate.sh
+++ log-rotate/log-rotate.sh
@@ -2,7 +2,7 @@
 
 logfile=/home/amandeep/shell-scripting/bash-scripting/log-rotate/logs/accesslogs.txt
 archivedir=/home/amandeep/shell-scripting/bash-scripting/log-rotate/log-archive
-maxsize=100000  #max size in bytes (1 mb)
+maxsize=10000  #max size in bytes (1 mb)
 
 logsize=$(stat -c%s "$logfile")
 echo "size of logfile" $logsize
@@ -10,6 +10,9 @@ if [ $logsize -ge $maxsize ]
 then
   echo "Log file is larger than maxsize"
   mv $logfile $archivedir
+  echo "Log file has been rotated"
+  echo "Creating new log file"
+  touch $logfile
 else
   echo "Log file is smaller than maxsize"
 fi
